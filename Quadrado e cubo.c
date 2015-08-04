a2d7f80681af1cac159f8642f24d133ef307da01
Luciabo <lucianoeduardodantas@gmail.com>
2015-08-04 14:56:26 -0300

    Variáveis, tipos e I/O
    <3 <3 <3

---
+++ Quadrado e cubo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+int a;
+scanf("%d", &a);
+printf("%d %d", a*a, a*a*a);
+return 0;
+}
---
+++ Quociente e resto da divisão.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(void)
+{
+int a;
+scanf("%d", &a);
+printf("%d %d", a/3, a%3);
+return 0;
+}
